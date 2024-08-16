3e6585d60744cf22548b91b18cb5dfa40f9dd837
Anderson <billbardales@gmail.com>
2024-08-16 17:12:39 -0500

    Modificar metodos

--- app.py
+++ app.py
@@ -6,7 +6,8 @@ app = Flask(__name__)
 def get_latest_data():
     conn = sqlite3.connect('weather_data.db')
     c = conn.cursor()
-    c.execute('''SELECT temperature, wind_speed, wind_direction, humidity, precipitation 
+    c.execute('''SELECT temperature, humidity, pressure, altitude, wind_direction, 
+                        wind_speed, precipitation, soil_humidity
                  FROM weather ORDER BY id DESC LIMIT 1''')
     data = c.fetchone()
     conn.close()
@@ -20,25 +21,35 @@ def index():
 def latest():
     data = get_latest_data()
     if data:
-        temperature, wind_speed, wind_direction, humidity, precipitation = data
+        (temperature, humidity, pressure, altitude, wind_direction, 
+         wind_speed, precipitation, soil_humidity) = data
     else:
-        temperature = wind_speed = wind_direction = humidity = precipitation = "N/A"
+        temperature = humidity = pressure = altitude = wind_direction = "N/A"
+        wind_speed = precipitation = soil_humidity = "N/A"
     return jsonify({
         "temperature": temperature,
-        "wind_speed": wind_speed,
-        "wind_direction": wind_direction,
         "humidity": humidity,
-        "precipitation": precipitation
+        "pressure": pressure,
+        "altitude": altitude,
+        "wind_direction": wind_direction,
+        "wind_speed": wind_speed,
+        "precipitation": precipitation,
+        "soil_humidity": soil_humidity
     })
 
 @app.route('/receive', methods=['POST'])
 def receive():
     data = request.form['data']
-    temperature, wind_speed, wind_direction, humidity, precipitation = data.split(';')
+    (temperature, humidity, pressure, altitude, wind_direction, 
+     wind_speed, precipitation, soil_humidity) = data.split(';')
     conn = sqlite3.connect('weather_data.db')
     c = conn.cursor()
-    c.execute('''INSERT INTO weather (temperature, wind_speed, wind_direction, humidity, precipitation)
-                 VALUES (?, ?, ?, ?, ?)''', (float(temperature), int(wind_speed), int(wind_direction), float(humidity), float(precipitation)))
+    c.execute('''INSERT INTO weather 
+                 (temperature, humidity, pressure, altitude, wind_direction, 
+                  wind_speed, precipitation, soil_humidity)
+                 VALUES (?, ?, ?, ?, ?, ?, ?, ?)''', 
+              (float(temperature), float(humidity), float(pressure), float(altitude), 
+               float(wind_direction), float(wind_speed), float(precipitation), float(soil_humidity)))
     conn.commit()
     conn.close()
     return 'Data received and stored'
